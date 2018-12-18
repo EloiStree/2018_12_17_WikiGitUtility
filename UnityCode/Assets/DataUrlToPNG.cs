75033ab6bdc215a49646ad08c45211c320db2bdf
Eloi Strée <jams.center@gmail.com>
2018-12-18 23:44:58 +0100

    Allow to download image from dataurl

--- UnityCode/Assets/DataUrlToPNG.cs
+++ UnityCode/Assets/DataUrlToPNG.cs
@@ -2,19 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class DataUrlToPNG : MonoBehaviour
 {
+    public Texture2D m_image;
+    [TextArea(1, 60)]
+    public string m_dataUrl;
     [TextArea(1,60)]
     public string m_data;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator DD()
     {
 
-        byte [] binData = Convert.FromBase64String(m_data);
-        File.WriteAllBytes(Application.dataPath + "/dd.png", binData);
 
+        m_data = Regex.Match(m_dataUrl, @"data:image/(?<type>.+?),(?<data>.+)").Groups["data"].Value;
+        
+        byte [] binData = Convert.FromBase64String(m_data);
+        m_dataUrl = "";
+        File.WriteAllBytes(Application.dataPath + "/../dd.png", binData);
+
+        WWW www = new WWW(Application.dataPath + "/../dd.png");
+        yield return www;
+        m_image = www.texture;
+
+        //m_image =new Texture2D(16, 16, TextureFormat.PVRTC_RGBA4, false);
+        //m_image.LoadRawTextureData(binData);
+        //m_image.Apply();
     }
-    
+
+
+    private void OnValidate()
+    {
+        if(m_dataUrl!="" )
+            StartCoroutine(DD());
+    }
 }
